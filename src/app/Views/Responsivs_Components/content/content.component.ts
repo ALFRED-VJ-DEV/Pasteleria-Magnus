ba3597cc4bec0cc07639a9ab80fe6a4d39336966
ALFRED-VJ-DEV <alfred.vj.dev@gmail.com>
2024-09-07 16:44:53 -0600

    imagenes responsiv footer responsiv

--- src/app/Views/Responsivs_Components/content/content.component.ts
+++ src/app/Views/Responsivs_Components/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BotonAcerPedidoComponent } from "../../Components/boton-acer-pedido/boton-acer-pedido.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [BotonAcerPedidoComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { NavbarComponent } from "./Views/Components/navbar/navbar.component";
 import { HomeComponent } from "./Views/Components/home/home.component";
 import { BotonAcerPedidoComponent } from "./Views/Components/boton-acer-pedido/boton-acer-pedido.component";
 import { MenuComponent } from "./Views/Responsivs_Components/menu/menu.component";
+import { ContentComponent } from "./Views/Responsivs_Components/content/content.component";
+import { FooterComponent } from "./Views/Responsivs_Components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    MatIconModule, NavbarComponent, HomeComponent, BotonAcerPedidoComponent, MenuComponent],
+    MatIconModule, NavbarComponent, HomeComponent, BotonAcerPedidoComponent, MenuComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
