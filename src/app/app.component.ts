fc597193390e57d29a432e572c057f8a46b4b73f
ALFRED-VJ-DEV <alfred.vj.dev@gmail.com>
2024-09-07 14:57:52 -0600

    imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule} from '@angular/material/icon';
 import { NavbarComponent } from "./Views/Components/navbar/navbar.component";
+import { HomeComponent } from "./Views/Components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    MatIconModule, NavbarComponent],
+    MatIconModule, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
