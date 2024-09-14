0b84bb42f771c47ee94f62b4f4a02c22138a38e6
ALFRED-VJ-DEV <alfred.vj.dev@gmail.com>
2024-09-14 16:37:41 -0600

    carrusel

--- src/app/Views/Responsivs_Components/sabores/sabores.component.ts
+++ src/app/Views/Responsivs_Components/sabores/sabores.component.ts
@@ -3,10 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sabores',
   standalone: true,
-  imports: [],
   templateUrl: './sabores.component.html',
-  styleUrl: './sabores.component.scss'
+  styleUrls: ['./sabores.component.scss']
 })
 export class SaboresComponent {
+  currentIndex = 0;
+  totalSlides = 2; // Número total de tarjetas
 
+  nextSlide() {
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    this.currentIndex = (this.currentIndex + 1) % this.totalSlides;
+    carousel.style.transform = `translateX(-${this.currentIndex * 100}%)`;
+  }
+
+  prevSlide() {
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    this.currentIndex = (this.currentIndex - 1 + this.totalSlides) % this.totalSlides;
+    carousel.style.transform = `translateX(-${this.currentIndex * 100}%)`;
+  }
 }
